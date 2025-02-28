fbc98122120fe842c3f0968d7f311c99e62fa190
Devin Olesen <devintanielu@gmail.com>
2025-02-28 04:12:08 +0000

    gets and post

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,14 @@
 import 'dotenv/config';
 import pg from 'pg';
 import express from 'express';
-import { ClientError, errorMiddleware } from './lib/index.js';
+import { ClientError, authMiddleware, errorMiddleware } from './lib/index.js';
+
+type Entry = {
+  entryId?: number;
+  title: string;
+  notes: string;
+  photoUrl: string;
+};
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
@@ -12,6 +19,66 @@ const db = new pg.Pool({
 });
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/api/entries', async (req, res, next) => {
+  try {
+    const sql = `
+    select * from "entries";
+    `;
+    const result = await db.query(sql);
+    const total = result.rows;
+    if (!total) {
+      throw new ClientError(404, `entries not found`);
+    }
+    res.json(total);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/entries/:entryId', async (req, res, next) => {
+  try {
+    const { entryId } = req.params;
+    if (entryId === undefined) {
+      throw new ClientError(400, `entryId required`);
+    }
+    const sql = `
+    select * from "entries"
+    where "entryId" = $1;
+    `;
+    const params = [entryId];
+    const result = await db.query<Entry>(sql, params);
+    const entry = result.rows[0];
+    if (!entry) {
+      throw new ClientError(404, `entry ${entryId} not found`);
+    }
+    res.json(entry);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/api/entries', async (req, res, next) => {
+  try {
+    const { title, notes, photoUrl } = req.body;
+    if (!title || !notes || !photoUrl) {
+      throw new ClientError(400, `title, notes and photoURL are required`);
+    }
+    const sql = `
+    insert into "entries" ("title", "notes", "photoUrl")
+    values ($1, $2, $3)
+    returning *;
+    `;
+    const params = [title, notes, photoUrl];
+    const result = await db.query(sql, params);
+    const entry = result.rows[0];
+    res.status(201).json(entry);
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`express server listening on port ${process.env.PORT}`);
